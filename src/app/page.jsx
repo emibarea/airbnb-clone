667e5e2dc070682ab706fe9778a7ffc0c0a3e623
Emiliano Barea <emilianobarea8@gmail.com>
2023-06-27 02:20:24 -0300

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,60 @@
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import SmallCard from "../components/SmallCard";
+import MediumCard from "../components/MediumCard";
+import LargeCard from "../components/LargeCard";
+import Footer from "../components/Footer";
+async function getData() {
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").then(
+    (res) => res.json()
+  );
+  const cardsData = await fetch("https://www.jsonkeeper.com/b/VHHT").then(
+    (res) => res.json()
+  );
+  return {
+    exploreData,
+    cardsData,
+  };
+}
+
+export default async function Page() {
+  const { exploreData, cardsData } = await getData();
+
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          {/* pull data from server - API endpoints */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ img, location, distance }) => (
+              <SmallCard
+                key={img}
+                image={img}
+                location={location}
+                distance={distance}
+              />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 ml-3">
+            {cardsData.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
+        </section>
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          buttonText="Get Inspired"
+          description="Wishlists curated by Airbnb."
+        />
+      </main>
+      <Footer />
+    </div>
+  );
+}
